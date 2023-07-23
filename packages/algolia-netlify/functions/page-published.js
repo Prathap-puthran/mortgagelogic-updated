b3b9ec01e01102288b04120b0f0bcc4ec6652e15
prathap puthran <4nm20me442@nmamit.in>
2023-07-23 20:41:01 +0530

    page index added

---
+++ packages/algolia-netlify/functions/page-published.js
@@ -0,0 +1,74 @@
+const IndexFactory = require('@tryghost/algolia-indexer');
+const transforms = require('@tryghost/algolia-fragmenter');
+
+exports.handler = async (event) => {
+    const {key} = event.queryStringParameters;
+
+    // TODO: Deprecate this in the future and make the key mandatory
+    if (key && key !== process.env.NETLIFY_KEY) {
+        return {
+            statusCode: 401,
+            body: `Unauthorized`
+        };
+    }
+
+    if (process.env.ALGOLIA_ACTIVE !== 'TRUE') {
+        return {
+            statusCode: 200,
+            body: `Algolia is not activated`
+        };
+    }
+
+    if (!event.headers['user-agent'].includes('https://github.com/TryGhost/Ghost')) {
+        return {
+            statusCode: 401,
+            body: `Unauthorized`
+        };
+    }
+
+    const algoliaSettings = {
+        appId: process.env.ALGOLIA_APP_ID,
+        apiKey: process.env.ALGOLIA_API_KEY,
+        index: process.env.ALGOLIA_INDEX
+    };
+
+    let {page} = JSON.parse(event.body);
+    page = (page && Object.keys(page.current).length > 0 && page.current) || {};
+
+    if (!page || Object.keys(page).length < 1) {
+        return {
+            statusCode: 200,
+            body: `No valid request body detected`
+        };
+    }
+
+    const node = [];
+
+    // Transformer methods need an Array of Objects
+    node.push(page);
+
+    // Transform into Algolia object with the properties we need
+    const algoliaObject = transforms.transformToAlgoliaObject(node);
+
+    // Create fragments of the page
+    const fragments = algoliaObject.reduce(transforms.fragmentTransformer, []);
+
+    try {
+        // Instanciate the Algolia indexer, which connects to Algolia and
+        // sets up the settings for the index.
+        const index = new IndexFactory(algoliaSettings);
+        await index.setSettingsForIndex();
+        await index.save(fragments);
+        console.log('Fragments successfully saved to Algolia index'); // eslint-disable-line no-console
+        return {
+            statusCode: 200,
+            body: `page "${page.title}" has been added to the index.`
+        };
+    } catch (error) {
+        console.log(error); // eslint-disable-line no-console
+        return {
+            statusCode: 500,
+            body: JSON.stringify({msg: error.message})
+        };
+    }
+};
---
+++ packages/algolia-netlify/functions/page-unpublished.js
@@ -0,0 +1,65 @@
+const IndexFactory = require('@tryghost/algolia-indexer');
+
+exports.handler = async (event) => {
+    const {key} = event.queryStringParameters;
+
+    // TODO: Deprecate this in the future and make the key mandatory
+    if (key && key !== process.env.NETLIFY_KEY) {
+        return {
+            statusCode: 401,
+            body: `Unauthorized`
+        };
+    }
+
+    if (process.env.ALGOLIA_ACTIVE !== 'TRUE') {
+        return {
+            statusCode: 200,
+            body: `Algolia is not activated`
+        };
+    }
+
+    if (!event.headers['user-agent'].includes('https://github.com/TryGhost/Ghost')) {
+        return {
+            statusCode: 401,
+            body: `Unauthorized`
+        };
+    }
+
+    const algoliaSettings = {
+        appId: process.env.ALGOLIA_APP_ID,
+        apiKey: process.env.ALGOLIA_API_KEY,
+        index: process.env.ALGOLIA_INDEX
+    };
+
+    const {page} = JSON.parse(event.body);
+
+    // Updated pages are in `page.current`, deleted are in `page.previous`
+    const {slug} = (page.current && Object.keys(page.current).length && page.current)
+                   || (page.previous && Object.keys(page.previous).length && page.previous);
+
+    if (!slug) {
+        return {
+            statusCode: 200,
+            body: `No valid request body detected`
+        };
+    }
+
+    try {
+        // Instanciate the Algolia indexer, which connects to Algolia and
+        // sets up the settings for the index.
+        const index = new IndexFactory(algoliaSettings);
+        await index.initIndex();
+        await index.delete(slug);
+        console.log(`Fragments for slug "${slug}" successfully removed from Algolia index`); // eslint-disable-line no-console
+        return {
+            statusCode: 200,
+            body: `page "${slug}" has been removed from the index.`
+        };
+    } catch (error) {
+        console.log(error); // eslint-disable-line no-console
+        return {
+            statusCode: 500,
+            body: JSON.stringify({msg: error.message})
+        };
+    }
+};
